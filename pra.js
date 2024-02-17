85defc72b8fb9221f46195ddcac8cca5be33f25c
peterjackson0107 <peterjackson0107@gmail.com>
2024-02-17 13:01:15 +0800

    20240217-1301

--- pra.js
+++ pra.js
@@ -1,6 +1,5 @@
 let obj = {};
 let obj1 = {};
-// let pict = document.getElementById("pict");
 
 fetch("https://api.data.metro.tokyo.lg.jp/v1/CulturalProperty?limit=1000")
   .then(response => response.json())
